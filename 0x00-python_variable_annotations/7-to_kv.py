2bc7a963e846d7683d465d6c293c95fbc4787e17
MarB-T <danmarbet@gmail.com>
2024-01-11 18:49:17 +0300

    using typing Union to accept either of two types and returning a tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''Type annotation where an argument can be either of two types'''
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Union[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''return a tuple of arguments passed'''
     return (k, float(v * v))

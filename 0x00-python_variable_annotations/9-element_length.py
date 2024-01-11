cc7c587b7fcbb84cfc933defbb3bb1c1061bed54
MarB-T <danmarbet@gmail.com>
2024-01-11 19:25:08 +0300

    using typing Iterable and Sequence correction

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """type annotation of iterables"""
-from typing import Tuple, List
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''return the lenght of iterables'''
     return [(i, len(i)) for i in lst]

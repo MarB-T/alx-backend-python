1249b53396e55d3adfff2a1bc7fb25f81130c6d9
MarB-T <danmarbet@gmail.com>
2024-01-11 15:02:02 +0300

    annotated func to return str representation of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''function to takes float and returns its string representation'''
+
+
+def to_str(n: float) -> str:
+    '''returns string representation of float'''
+    return str(n)

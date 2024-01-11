63b0038fa0cb7acb9beeb20a82f369814b12437e
MarB-T <danmarbet@gmail.com>
2024-01-11 19:19:50 +0300

    using typing Iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """type annotation of iterables"""
-from typing import Iterable, Tuple, List
+from typing import Tuple, List
 
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:

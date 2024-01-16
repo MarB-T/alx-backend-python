1858619501290290e1cbb259b0aa3998463b1205
MarB-T <danmarbet@gmail.com>
2024-01-16 20:32:13 +0300

    async comprehension correction

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Async comrehension"""
 import asyncio
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     '''async comrehension'''
     result = [num async for num in async_generator()]
     return result

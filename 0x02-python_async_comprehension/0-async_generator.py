85102289ec44ed87966ee5fa67c47ebb9971f7d2
MarB-T <danmarbet@gmail.com>
2024-01-16 19:48:44 +0300

    async function using yield correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> float:
     '''ansynchronoi=us random generator'''
     for _ in range(10):
         await asyncio.sleep(1)
